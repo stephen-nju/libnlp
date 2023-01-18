a1f5e5996c698b526ac3705c8d542f9dcc98b1f3
stephen-nju <zhubinnju@outlook.com>
2023-01-18 22:39:56 +0800

    010101

--- test/test_strutils.c
+++ test/test_strutils.c
@@ -2,14 +2,18 @@
 #include "strutils.h"
 #include "utf8proc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 SUITE(libnlp_strutils_tests);
 
 TEST test_libnlp(void)
 {
     const nlp_uint8_t *s="abcde中国";
-    // bool ret=utf8str_chr(s,42);
-    // printf("%d",ret);
+    nlp_int32_t *dst=(nlp_int32_t *) malloc(sizeof(nlp_int32_t));
+    nlp_size_t d=utf8str_iterate("中",-1,dst);
+    printf("%zu",d);
+    nlp_uint8_t* ret=utf8str_chr(s,*dst);
+    printf("%s",ret);
     PASS();
 }
 SUITE(libnlp_strutils_tests) { RUN_TEST(test_libnlp); }
